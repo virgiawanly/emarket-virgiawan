7bb73c6a0c89b73c07a35ff23724b6776baa8dac
Virgiawan Listiyandi <imvirgiawan17@gmail.com>
2021-10-14 16:38:41 +0700

    Admin Template & Migration

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('admin.layouts.main')
+
+@section('content-header')
+    <h1>Dashboard</h1>
+@endsection
+
+@section('content')
+    <h1>Hello World</h1>
+@endsection
---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,52 @@
+<div class="main-sidebar">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html">E-Market</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">EM</a>
+        </div>
+        <ul class="sidebar-menu">
+            <!-- Dashboard -->
+            <li class="menu-header">Dashboard</li>
+            <li class="active"><a class="nav-link" href="/"><i
+                        class="fas fa-fire"></i><span>Dashboard</span></a></li>
+
+            <!-- Master -->
+            <li class="menu-header">Master</li>
+            <li><a class="nav-link" href="/produk"><i class="fas fa-box-open"></i>
+                    <span>Produk</span></a></li>
+            <li><a class="nav-link" href="/barang"><i class="fas fa-boxes"></i>
+                    <span>Barang</span></a></li>
+            <li><a class="nav-link" href="/pelanggan"><i class="fas fa-user"></i>
+                    <span>Pelanggan</span></a></li>
+            <li><a class="nav-link" href="/supplier"><i class="fas fa-truck"></i>
+                    <span>Supplier</span></a></li>
+
+            <!-- Transaction Area -->
+            <li class="menu-header">Transaksi</li>
+            <li><a class="nav-link" href="/pembelian"><i class="fas fa-download"></i>
+                    <span>Pembelian</span></a></li>
+            <li><a class="nav-link" href="/penjualan"><i class="fas fa-upload"></i>
+                    <span>Penjualan</span></a></li>
+            <li><a class="nav-link" href="/transaksi-baru"><i class="fas fa-cash-register"></i>
+                    <span>Transaksi Baru</span></a></li>
+
+            <!-- Utility Area -->
+            <li class="menu-header">Utilitas</li>
+            <li><a class="nav-link" href="/"><i class="fas fa-users"></i>
+                    <span>Staff</span></a></li>
+            <li><a class="nav-link" href="/"><i class="fas fa-address-card"></i>
+                    <span>Profile</span></a></li>
+            <li><a class="nav-link" href="/"><i class="fas fa-cog"></i>
+                    <span>Pengaturan</span></a></li>
+
+            <!-- Others -->
+            <li class="menu-header">Lainnya</li>
+            <li><a class="nav-link" href="/"><i class="fas fa-info"></i>
+                    <span>Tentang Program</span></a></li>
+            <li class="mb-4"><a class="nav-link" href="/"><i class="fas fa-sign-out-alt"></i>
+                    <span>Logout</span></a></li>
+        </ul>
+    </aside>
+</div>
